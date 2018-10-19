dabe5408e104ca7e2ca0d5a2e26944370aa9e1ef
uchachaduneli@gmail.com <apokaliptika123>
2018-10-19 18:59:14 +0400

    first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -37,15 +37,16 @@ public class Main {
     pageSize.setW(BigInteger.valueOf(16840));
     pageSize.setH(BigInteger.valueOf(11900));
 
-    CTSectPr sectPr = document.getDocument().getBody().addNewSectPr();
-    CTPageMar pageMar = sectPr.addNewPgMar();
-    pageMar.setLeft(BigInteger.valueOf(0L));
-    pageMar.setTop(BigInteger.valueOf(0L));
-    pageMar.setRight(BigInteger.valueOf(0L));
-    pageMar.setBottom(BigInteger.valueOf(0L));
+    // მარჯინების დასმა
+//    CTSectPr sectPr = document.getDocument().getBody().addNewSectPr();
+//    CTPageMar pageMar = sectPr.addNewPgMar();
+//    pageMar.setLeft(BigInteger.valueOf(10L));
+//    pageMar.setTop(BigInteger.valueOf(10L));
+//    pageMar.setRight(BigInteger.valueOf(10L));
+//    pageMar.setBottom(BigInteger.valueOf(10L));
 
     //Write the Document in file system
-    FileOutputStream out = new FileOutputStream(new File("C:\\Users\\home\\Desktop\\myDoc.docx"));
+    FileOutputStream out = new FileOutputStream(new File("C:\\Users\\ucha.chaduneli\\Desktop\\myDoc.docx"));
 
     //create Paragraph
     XWPFParagraph paragraph = document.createParagraph();
@@ -57,56 +58,55 @@ public class Main {
     if (headerFooterPolicy == null) headerFooterPolicy = document.createHeaderFooterPolicy();
 
     // create header start
-    XWPFHeader header = headerFooterPolicy.createHeader(XWPFHeaderFooterPolicy.FIRST);
+    XWPFHeader header = headerFooterPolicy.createHeader(XWPFHeaderFooterPolicy.DEFAULT);
 
     headerParagraph = header.createParagraph();
-    headerParagraph.setAlignment(ParagraphAlignment.CENTER);
-
+    headerParagraph.setAlignment(ParagraphAlignment.LEFT);
     run = headerParagraph.createRun();
-    run.setText("The Header:");
+    run.setText("MIMINO TRAVEL GEORGIA");
 
-    File imgFile = new File("C:\\Users\\home\\IdeaProjects\\poi\\src\\main\\resources\\background.png");
+    File imgFile = new File("C:\\Users\\ucha.chaduneli\\IdeaProjects\\poi\\src\\main\\resources\\background.png");
     Dimension dim = getImageDimension(imgFile);
     double width = dim.getWidth();
     double height = dim.getHeight();
 
     double scaling = 1.0;
-    if (width > 82 * 10.3) scaling = (82 * 10.3) / width; //scale width not to be greater than 6 inches
+    if (width > 50 * 7) scaling = (50 * 7) / width; //scale width not to be greater than 6 inches
+    if (height > 40 * 7) scaling = (40 * 7) / height;
     InputStream in = new FileInputStream(imgFile);
-    paragraph.setAlignment(ParagraphAlignment.BOTH);
+    paragraph.setAlignment(ParagraphAlignment.CENTER);
     paragraph.createRun().addPicture(in, Document.PICTURE_TYPE_PNG, "background.png",
             Units.toEMU(width * scaling), Units.toEMU(height * scaling));
     in.close();
 
-
-//    textbox
-//    CTGroup ctGroup = CTGroup.Factory.newInstance();
-//    CTShape ctShape = ctGroup.addNewShape();
-//    ctShape.setStyle("width:300pt;height:100pt");
-//    CTTxbxContent ctTxbxContent = ctShape.addNewTextbox().addNewTxbxContent();
-//    ctTxbxContent.addNewP().addNewR().addNewT().setStringValue("The TextBox text...");
-//    Node ctGroupNode = ctGroup.getDomNode();
-//    CTPicture ctPicture = CTPicture.Factory.parse(ctGroupNode);
-//    run=paragraph.createRun();
-//    CTR cTR = run.getCTR();
-//    cTR.addNewPict();
-//    cTR.setPictArray(0, ctPicture);
-//    *************
-
-
-//    run = paragraph.createRun();
-//    run.addBreak(BreakType.PAGE);
-//    run.addBreak(BreakType.TEXT_WRAPPING);
-//    run.setText("Main Text babli bubli adlkasj;la doai;sjd as;dj asl;djas ldjald jaslj\n");
+//    File imgFile = new File("C:\\Users\\ucha.chaduneli\\IdeaProjects\\poi\\src\\main\\resources\\background.png");
+//    Dimension dim = getImageDimension(imgFile);
+//    double width = dim.getWidth();
+//    double height = dim.getHeight();
+//
+//    double scaling = 1.0;
+//    if (width > 82 * 10.3) scaling = (82 * 10.3) / width; //scale width not to be greater than 6 inches
+//    InputStream in = new FileInputStream(imgFile);
+//    paragraph.setAlignment(ParagraphAlignment.BOTH);
+//    paragraph.createRun().addPicture(in, Document.PICTURE_TYPE_PNG, "background.png",
+//            Units.toEMU(width * scaling), Units.toEMU(height * scaling));
+//    in.close();
 
     // create footer start
     XWPFFooter footer = headerFooterPolicy.createFooter(XWPFHeaderFooterPolicy.DEFAULT);
 
     paragraph = footer.createParagraph();
-    paragraph.setAlignment(ParagraphAlignment.LEFT);
-
+    paragraph.setAlignment(ParagraphAlignment.BOTH);
+    paragraph.setBorderTop(Borders.THICK);
     run = paragraph.createRun();
-    run.setText("The Footer: ");
+    run.setText("The Footer ტექსტ: ");
+
+
+    paragraph = document.createParagraph();
+    run = paragraph.createRun();
+    run.setText("Breakfast at the hotel. Early in the morning start very scenic drive up through the High Caucasus Mountains along the Georgian Military Highway towards Kazbegi region. On the way we will drive across the Cross Pass (2 395m.) and along the Tergi River that brings us to Kazbegi – main town in this region. From the centre of Kazbegi drive by 4x4 through beautiful valleys and woodland leads us to Gergeti Holy Trinity church (14th century), stunningly located on a hilltop (2170 m.) ");
+    run.setText("Breakfast at the hotel. Early in the morning start very scenic drive up through the High Caucasus Mountains along the Georgian Military Highway towards Kazbegi region. On the way we will drive across the Cross Pass (2 395m.) and along the Tergi River that brings us to Kazbegi – main town in this region. From the centre of Kazbegi drive by 4x4 through beautiful valleys and woodland leads us to Gergeti Holy Trinity church (14th century), stunningly located on a hilltop (2170 m.) ");
+    run.setText("Breakfast at the hotel. Early in the morning start very scenic drive up through the High Caucasus Mountains along the Georgian Military Highway towards Kazbegi region. On the way we will drive across the Cross Pass (2 395m.) and along the Tergi River that brings us to Kazbegi – main town in this region. From the centre of Kazbegi drive by 4x4 through beautiful valleys and woodland leads us to Gergeti Holy Trinity church (14th century), stunningly located on a hilltop (2170 m.) ");
 
     document.write(out);
 
@@ -114,7 +114,7 @@ public class Main {
     out.close();
     System.out.println("Doc Generated successfully");
     if (Desktop.isDesktopSupported()) {
-      Desktop.getDesktop().open(new File("C:\\Users\\home\\Desktop\\myDoc.docx"));
+      Desktop.getDesktop().open(new File("C:\\Users\\ucha.chaduneli\\Desktop\\myDoc.docx"));
     }
   }
 }
